3e0e20e31add64bb9c26def5de6a2895575f65ca
anton <russiansoveet@gmail.com>
2026-04-14 22:25:52 +0300

    fix: fix tests and ci postgres connectivity

--- src/test/java/com/antonk404/poker_rating/PokerRatingApplicationTests.java
+++ src/test/java/com/antonk404/poker_rating/PokerRatingApplicationTests.java
@@ -2,8 +2,12 @@ package com.antonk404.poker_rating;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1)
+@TestPropertySource(properties = "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}")
 class PokerRatingApplicationTests {
 
 	@Test
